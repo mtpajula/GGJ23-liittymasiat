f50d0f2ba1b362309b383654b133e10f33d7a02d
Mikko Pajula <mtpajula@gmail.com>
2023-02-05 14:36:49 +0200

    end screen

--- lapland_defence/game_objects/end/credits.py
+++ lapland_defence/game_objects/end/credits.py
@@ -1,16 +1,39 @@
-from engine.components.types.text_object import TextObject
+from engine.components.game_object import GameObject
 from engine.main_game import MainGame
 
 
-class Credits(TextObject):
+class Credits(GameObject):
 
     def __init__(self):
-        super().__init__('Lapland 2053 defense\nasdfa')
-        self.heading = False
+        super().__init__()
+        self.texts = [
+            "Credits",
+            "",
+            "Mikko P - Game Design / Programming",
+            "sokuni - Game Design / Music / Voice acting",
+            "Siiri P - Game Design / Art",
+            "AI / Stable diffusion 2.1 - Intro art",
+            "",
+            "Special thanks:",
+            "",
+            "Pettis - Additional programming / retro sound FX",
+            "",
+            "The game music uses the sample:",
+            "Sandyrb Tubular Bells 02",
+            " -> TUBULAR 007.wav @ Freesounds.org",
+            "The original sample has been transposed to E",
+            "and slightly cut from the end."
+        ]
+        self.surfaces = []
 
     def start(self, main_game: MainGame):
         super().start(main_game)
         self.position = main_game.screen.location(left=600, top=300)
+        self.surfaces = []
+        for text in self.texts:
+            self.surfaces.append(main_game.font.render(text, False, (255, 255, 255)))
 
     def draw(self, main_game: MainGame):
         super().draw(main_game)
+        for i, surface in enumerate(self.surfaces):
+            main_game.window.blit(surface, (self.position[0], self.position[1] + i*20))
